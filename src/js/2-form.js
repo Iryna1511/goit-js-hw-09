c85a5cf5364ae89f54fc6fe0f18d4d9e54284ca0
Iryna1511 <irina.shkolnikova93@gmail.com>
2024-03-16 08:33:28 +0200

    working on  form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,4 +1,29 @@
 const formEl = document.querySelector('.feedback-form');
 const LS_KEY = 'feedback-form-state';
-const email = formEl.elements.email;
-const message = formEl.elements.message;
+const emailInp = formEl.elements.email;
+const messageInp = formEl.elements.message;
+const user = {};
+const data = {};
+
+formEl.addEventListener('input', handleInput);
+function handleInput(event) {
+  data.email = emailInp.value.trim();
+  data.message = messageInp.value.trim();
+  localStorage.setItem(LS_KEY, JSON.stringify(data));
+}
+emailInp.value = JSON.parse(localStorage.getItem(LS_KEY.email)) ?? '';
+messageInp.value = JSON.parse(localStorage.getItem(LS_KEY.message)) ?? '';
+
+formEl.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  localStorage.removeItem(LS_KEY);
+  user.email = emailInp.value.trim();
+  user.message = messageInp.value.trim();
+  if (user.email === '' || user.message === '') {
+    alert('All form fields must be filled in');
+  } else {
+    console.log(user);
+    formEl.reset();
+  }
+}
