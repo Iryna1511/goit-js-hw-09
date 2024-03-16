d57bcfbda8ba38ac670b4ecd6a9dd1816646ebc2
Iryna1511 <irina.shkolnikova93@gmail.com>
2024-03-16 15:12:05 +0200

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -11,19 +11,26 @@ function handleInput(event) {
   data.message = messageInp.value.trim();
   localStorage.setItem(LS_KEY, JSON.stringify(data));
 }
-emailInp.value = JSON.parse(localStorage.getItem(LS_KEY.email)) ?? '';
-messageInp.value = JSON.parse(localStorage.getItem(LS_KEY.message)) ?? '';
+
+function loadFormState(key) {
+    const parsedInfo = JSON.parse(localStorage.getItem(key));
+    console.log(parsedInfo);
+//   emailInp.value = JSON.parse(localStorage.getItem(LS_KEY.email)) ?? '';
+//   messageInp.value = JSON.parse(localStorage.getItem(LS_KEY.message)) ?? '';
+}
+loadFormState(LS_KEY);
 
 formEl.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
-  event.preventDefault();
-  localStorage.removeItem(LS_KEY);
-  user.email = emailInp.value.trim();
-  user.message = messageInp.value.trim();
   if (user.email === '' || user.message === '') {
     alert('All form fields must be filled in');
+    return;
   } else {
-    console.log(user);
-    formEl.reset();
+    event.preventDefault();
+    user.email = emailInp.value.trim();
+    user.message = messageInp.value.trim();
+    localStorage.removeItem(LS_KEY);
+      formEl.reset();
+      return user;
   }
 }
